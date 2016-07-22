ed58611d6cec169b8465ef648976bd8efc47b028
Henry Stern <stern@fsi.io>
2016-07-22 10:03:10 -0300

    main: decode raw bytes to utf-8 for printing in python3

--- axamd/client/__main__.py
+++ axamd/client/__main__.py
@@ -156,13 +156,13 @@ def main():
         elif args.channels:
             for result in client.sra(args.channels, args.watches,
                     timeout=timeout, **client_args):
-                print (result)
+                print (result.decode('utf-8'))
                 sys.stdout.flush()
         elif args.anomaly:
             anomaly = Anomaly(args.anomaly[0], watches=args.watches,
                     options=' '.join(args.anomaly[1:]))
             for result in client.rad([anomaly], timeout=timeout, **client_args):
-                print (result)
+                sys.stdout.write(result.decode('utf-8'))
                 sys.stdout.flush()
         else:
             parser.error('Need channels and watches for SRA mode or anomaly for RAD mode')
