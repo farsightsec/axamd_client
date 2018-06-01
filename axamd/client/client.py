4961a967690d6c648ae32e6449a1858b95f9b6fe
jeff murphy <jcmurphy@jeffmurphy.org>
2018-06-01 14:39:47 -0400

    remove retry-on-404.
    axamd returns 404 to indicate that, eg, a channel or anomaly does
    not exist in the user config. we don't want to retry on those cases,
    we instead want to immediately fail and let the user know.

--- axamd/client/client.py
+++ axamd/client/client.py
@@ -71,7 +71,7 @@ except ImportError:
 def requests_retry_session(
     retries=3,
     backoff_factor=0.3,
-    status_forcelist=(404, 500, 502, 504),
+    status_forcelist=(500, 502, 504),
     session=None,
 ):
     session = session or requests.Session()
