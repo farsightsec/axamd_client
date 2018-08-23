edeaf6cacba90cae442ca71fd5079268ae45604b
Jeff Murphy <jeff@fsi.io>
2018-08-23 12:59:32 -0400

    iterator instead of tuple

--- axamd/client/__main__.py
+++ axamd/client/__main__.py
@@ -200,13 +200,13 @@ def main():
         elif args.channels:
             for result in client.sra(args.channels, args.watches,
                     timeout=timeout, **client_args):
-                print (result.decode('utf-8'))
+                print (result)
                 sys.stdout.flush()
         elif args.anomaly:
             anomaly = Anomaly(args.anomaly[0], watches=args.watches,
                     options=' '.join(args.anomaly[1:]))
             for result in client.rad([anomaly], timeout=timeout, **client_args):
-                print (result.decode('utf-8'))
+                print (result)
                 sys.stdout.flush()
         else:
             parser.error('Need channels and watches for SRA mode or anomaly for RAD mode')
